f6868e5d612c641d1bcba1d67bfc8867df1ccd37
Gabszzz05 <gabszzz005@gmail.com>
2024-09-25 14:09:57 -0500

    Creacion proyecto frontend

---
+++ src/test/java/pe/edu/cibertec/Autos_FrontEnd/AutosFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Autos_FrontEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
